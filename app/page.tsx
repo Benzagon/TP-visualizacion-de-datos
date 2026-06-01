6df52b026eb3d4521771a34c2fa15d7624dfd9c6
Benzagon <gonbenzaquen@gmail.com>
2026-06-01 20:51:45 -0300

    fix: remove broken fikes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 import Mapas from "./components/Mapas";
 import Produccion from "./components/Produccion";
 
-export default function Home() {
+export default function Page() {
   return (
     <>
       <Hero />
