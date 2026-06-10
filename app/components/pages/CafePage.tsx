7b11aff9fda530d7d8fd6b01a1d95e5423c18902
Benzagon <gonbenzaquen@gmail.com>
2026-06-10 10:54:51 -0300

    cafe paralax

--- app/components/pages/CafePage.tsx
+++ app/components/pages/CafePage.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import CafeSection from '../CafeSection'
 import { MasTomanCafe } from '../MasToman'
 import { ProduccionCafe } from '../Produccion'
+import ParalaxSeparator from '../ParalaxSeparator'
 
 const CafePage = () => {
   return (
     <>
         <CafeSection></CafeSection>
         <MasTomanCafe />
+        <ParalaxSeparator title="La producción" image="/cafe_prod.jpg" />
         <ProduccionCafe />
+        <ParalaxSeparator title="El consumo" image="/cafe_consumo.jpg" />
     </>
   )
 }
