3c8d4c525211221308f222a0e6ea62f8143cfa58
Benzagon <gonbenzaquen@gmail.com>
2026-06-17 09:26:08 -0300

    bajar tipos de mate

--- app/components/pages/MatePage.tsx
+++ app/components/pages/MatePage.tsx
@@ -9,11 +9,11 @@ const MatePage = () => {
   return (
     <>
         <MateSection />
+        <ParalaxSeparator title="La producción" image="/mate_prod.jpg" />
+        <ProduccionMate />
         <ParalaxSeparator title="Los tipos de Mate" image="/tipos_mate.jpg" />
         <TiposDeMate />
         <MasTomanMate />
-        <ParalaxSeparator title="La producción" image="/mate_prod.jpg" />
-        <ProduccionMate />
         <ParalaxSeparator title="El consumo" image="/mate_consumo.jpg" />
     </>
 )
