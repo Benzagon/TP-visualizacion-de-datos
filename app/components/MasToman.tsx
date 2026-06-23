2491441e13e24fc16f0e54bed5c0f44d6b2f5fa4
Benzagon <gonbenzaquen@gmail.com>
2026-06-23 20:30:06 -0300

    fondo

--- app/components/MasToman.tsx
+++ app/components/MasToman.tsx
@@ -4,7 +4,7 @@ import FlourishChart from "./FlourishChart";
 
 const MasTomanMate = () => {
   return (
-    <section className="w-full bg-surface border-t border-border">
+    <section className="w-full bg-background border-t border-border">
       <div className="max-w-7xl mx-auto px-6 md:px-10 pt-20 pb-8">
         <Title>Predomina la dulzura</Title>
         <div className="">
